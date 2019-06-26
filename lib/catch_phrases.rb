ce5750c17f27dbfc5f6d453a03b62ae690e010ae
Louis Lammerting <louis.lammerting@gmail.com>
2019-06-26 22:33:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,20 @@
-phrase = "It's-a me, Mario!"
+
 status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
   puts status
+end]
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def any_phrase
+  phrase = "Do A Barrel Roll!"
+  puts phrase
 end
